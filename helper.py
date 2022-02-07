1a275734c7af24938a4572ed3961cf7281c24c93
Mehran Zeidi <74262871+MehranZdi@users.noreply.github.com>
2022-02-07 23:28:42 +0330

    final helper.py module for streamlit was added.

--- helper.py
+++ helper.py
@@ -1,22 +1,23 @@
 import tensorflow as tf
 import cv2
-import os
-import FileMaker as fm
+from skimage.util import random_noise
+import numpy as np
+
+model = tf.keras.models.load_model(
+        r'/home/mehranz/PycharmProjects/Denoising_streamlit/static/DenoiserModel.h5')
 
 
-current_path = os.getcwd()
-model = tf.keras.models.load_model(r'/home/mehranz/PycharmProjects/Denoising_streamlit/static/FaceUnetDenoising.h5')
-test_path = '/home/mehranz/Documents/Datasets/Denoising_face/FileMakerModule_images'
+def predictor(uploaded_image_path):
 
-
-def predictor(path, uploaded_image):
-
-    files = fm.File_Maker(test_path)
-    images = files.file_maker()
-    dicti = files.list_to_dict(images)
-    uploaded_image = cv2.resize(uploaded_image, (256, 256))
-    dicti['uploaded'] = uploaded_image
-    images.append(uploaded_image)
-    predicted = model.predict(images)
+    uploaded_image = cv2.imread(uploaded_image_path)
+    uploaded_image = cv2.resize(uploaded_image, (512, 512))
+    uploaded_image = random_noise(uploaded_image, mode='s&p', amount=0)
+    uploaded_image = tf.expand_dims(uploaded_image, axis=0)
+    predicted = model.predict(uploaded_image)
+    predicted = tf.squeeze(predicted, axis=0)
+    predicted = np.array(predicted)
+    predicted = cv2.cvtColor(predicted, cv2.COLOR_BGR2RGB)
+    predicted = np.array(predicted)
+    pil_img = tf.keras.preprocessing.image.array_to_img(predicted)
 
     return predicted
